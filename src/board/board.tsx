2de4e981d7ba122a00cff6314057683be08fc402
jbrischle <4125672+jbrischle@users.noreply.github.com>
2023-12-19 11:25:16 +0100

    add restart button

--- src/board/board.tsx
+++ src/board/board.tsx
@@ -1,13 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {Card} from '../card/card.tsx';
-import {StyleSheet, TouchableHighlight, View} from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  TouchableHighlight,
+  useColorScheme,
+  View,
+} from 'react-native';
 
 export default function Board({list}: {list: string[]}): React.JSX.Element {
-  const [currentTurn, setCurrentTurn] = useState({} as Record<number, string>);
-  const [lastRevealedPair, setLastRevealedPair] = useState(
-    {} as Record<number, string>,
-  );
-  const [revealedPairs, setRevealedPairs] = useState(
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const [score, setScore] = useState<[number, number]>([0, 0]);
+  const [currentTurn, setCurrentTurn] = useState<Record<number, string>>({});
+  const [lastRevealedPair, setLastRevealedPair] = useState({});
+  const [revealedPairs, setRevealedPairs] = useState<Record<number, string>>(
     {} as Record<number, string>,
   );
 
@@ -20,6 +27,13 @@ export default function Board({list}: {list: string[]}): React.JSX.Element {
       setCurrentTurn({});
     }
   }, [currentTurn, revealedPairs]);
+
+  function reset() {
+    setCurrentTurn({});
+    setLastRevealedPair({});
+    setRevealedPairs({});
+    setScore([0, 0]);
+  }
 
   function onClick(entry: string, index: number) {
     const local = {...currentTurn};
@@ -49,26 +63,40 @@ export default function Board({list}: {list: string[]}): React.JSX.Element {
   }
 
   return (
-    <View style={styles.grid}>
-      {list.map((pokemonId, index) => (
-        <TouchableHighlight
-          key={index}
-          onPress={() => onClick(pokemonId, index)}
-          disabled={isAlreadyRevealed(index) || isInCurrentTurn(index)}>
-          <Card
-            id={pokemonId}
-            showBack={shouldShowBack(index)}
-            current={isInCurrentTurn(index)}
-            revealed={isAlreadyRevealed(index)}
-          />
-        </TouchableHighlight>
-      ))}
+    <View>
+      <View style={styles.statusBar}>
+        {/*<Text style={{color: isDarkMode ? Colors.lighter : Colors.darker}}>*/}
+        {/*  {score[0]}:{score[1]}*/}
+        {/*</Text>*/}
+        <Button title={'Restart'} onPress={() => reset()} />
+      </View>
+      <View style={styles.grid}>
+        {list.map((pokemonId, index) => (
+          <TouchableHighlight
+            key={index}
+            onPress={() => onClick(pokemonId, index)}
+            disabled={isAlreadyRevealed(index) || isInCurrentTurn(index)}>
+            <Card
+              id={pokemonId}
+              showBack={shouldShowBack(index)}
+              current={isInCurrentTurn(index)}
+              revealed={isAlreadyRevealed(index)}
+            />
+          </TouchableHighlight>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  score: {},
+  statusBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'center',
+    gap: 50,
+  },
   grid: {
     display: 'flex',
     flexDirection: 'row',
