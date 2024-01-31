7bc75abf1b0077f4a522636b8dbc21a1c25c84dd
jbrischle <janick.brischle+github@gmail.com>
2024-01-31 18:19:09 +0100

    fix: init of translation capabilities

--- i18n.js
+++ i18n.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import {ReactNativeLanguageDetector} from 'react-native-localization-settings';
 
 const resources = {
   de: {
@@ -14,15 +13,12 @@ const resources = {
     },
   },
 };
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'de',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-export default i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(ReactNativeLanguageDetector)
-  .init({
-    resources,
-    compatibilityJSON: 'v3',
-    fallbackLng: 'de',
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  });
+export default i18n;
